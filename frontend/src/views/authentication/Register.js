b9ba856c4060c37c29164d195bd67e0daa4be727
Minal Khona <mn977442@dal.ca>
2022-07-14 00:42:54 -0300

    Authentication Module Changes

--- frontend/src/views/authentication/Register.js
+++ frontend/src/views/authentication/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './Auth.css'
 import { useNavigate } from 'react-router-dom';
-import { Button, Grid, TextField, Typography, Box, Container } from "@mui/material";
+import { Button, Grid, TextField, Typography, Box, Container, Paper } from "@mui/material";
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 import UserPool from "./UserPool"
 
@@ -148,146 +148,152 @@ const Register = () => {
   }
 
   return (
-    <Container component="main" maxWidth="xs" sx={{
+    <Container component="main" maxWidth="sm" sx={{
       marginTop: 4,
       marginBottom: 4
     }}>
-      <Typography textAlign={'center'} variant={'h6'}>
-        REGISTRATION
-      </Typography>
-      <hr />
-
-      {/* <div className="form-group"> */}
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column'
-        }}
-        component="form" noValidate onSubmit={handleSubmit}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              type="text"
-              id="given_name"
-              name="given_name"
-              label="First Name"
-              required
-              fullWidth
-              onChange={onFormChange}
-            />
-            {formErrors?.given_name?.required && <Typography color="red" variant="body2">First name is required!</Typography>}
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              type="text"
-              id="family_name"
-              name="family_name"
-              label="Last Name"
-              required
-              fullWidth
-              onChange={onFormChange}
-            />
-            {formErrors?.family_name?.required && <Typography color="red" variant="body2">Last name is required!</Typography>}
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              type="text"
-              id="email"
-              name="email"
-              label="Email"
-              required
-              fullWidth
-              onChange={onFormChange}
-            />
-            {formErrors?.email?.required && <Typography color="red" variant="body2">Email is required!</Typography>}
-            {formErrors?.email?.valid && <Typography color="red" variant="body2">Email is invalid!</Typography>}
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              type="password"
-              id="password"
-              name="password"
-              label="Password"
-              required
-              fullWidth
-              onChange={onFormChange}
-            />
-            {formErrors?.password?.required && <Typography color="red" variant="body2">Password is required!</Typography>}
-            {formErrors?.password?.valid && <Typography color="red" variant="body2">Password must contain numbers, special characters, uppercase letters and lowercase letters</Typography>}
-            {formErrors?.password?.minLength && <Typography color="red" variant="body2">Password length must be 8 characters</Typography>}
-          </Grid>
-
-          <Grid item xs={12}>
-            <Typography textAlign={'center'} variant={'h6'}>
-              SECURITY QUESTIONS
+      <Paper sx={{ p: 2, mt: 2, borderBlockColor: 'blue', border: 1 }}>
+        <Box
+          sx={{
+            marginTop: 2,
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+          component="form" noValidate onSubmit={handleSubmit}>
+          <Paper sx={{ p: 2, mt: 2 }}>
+            <Typography textAlign={'center'} variant={'h6'} sx={{ backgroundColor: 'lightblue' }}>
+              REGISTRATION
             </Typography>
             <hr />
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <label>What is the name of your favorite color?</label>
-          <input
-            className="form-control"
-            type="text"
-            id="answer1"
-            name="answer1"
-            required
-            placeholder="Favorite Color"
-            style={{ margin: "0.75rem 0 0.75rem 0" }}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <label>Who is your role model?</label>
-          <input
-            className="form-control"
-            type="text"
-            id="answer2"
-            name="answer2"
-            required
-            placeholder="Role Model"
-            style={{ margin: "0.75rem 0 0.75rem 0" }}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <label>Who is your favorite player?</label>
-          <input
-            className="form-control"
-            type="text"
-            id="answer3"
-            name="answer3"
-            required
-            placeholder="Favorite Player"
-            style={{ margin: "0.75rem 0 0.75rem 0" }}
-          />
-        </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  type="text"
+                  id="given_name"
+                  name="given_name"
+                  label="First Name"
+                  required
+                  fullWidth
+                  onChange={onFormChange}
+                />
+                {formErrors?.given_name?.required && <Typography color="red" variant="body2">First name is required!</Typography>}
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  type="text"
+                  id="family_name"
+                  name="family_name"
+                  label="Last Name"
+                  required
+                  fullWidth
+                  onChange={onFormChange}
+                />
+                {formErrors?.family_name?.required && <Typography color="red" variant="body2">Last name is required!</Typography>}
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  type="email"
+                  id="email"
+                  name="email"
+                  label="Email"
+                  required
+                  fullWidth
+                  onChange={onFormChange}
+                />
+                {formErrors?.email?.required && <Typography color="red" variant="body2">Email is required!</Typography>}
+                {formErrors?.email?.valid && <Typography color="red" variant="body2">Email is invalid!</Typography>}
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  type="password"
+                  id="password"
+                  name="password"
+                  label="Password"
+                  required
+                  fullWidth
+                  onChange={onFormChange}
+                />
+                {formErrors?.password?.required && <Typography color="red" variant="body2">Password is required!</Typography>}
+                {formErrors?.password?.valid && <Typography color="red" variant="body2">Password must contain numbers, special characters, uppercase letters and lowercase letters</Typography>}
+                {formErrors?.password?.minLength && <Typography color="red" variant="body2">Password length must be 8 characters</Typography>}
+              </Grid>
+            </Grid>
+          </Paper>
 
-        <Grid item xs={12}>
-          <Typography textAlign={'center'} variant={'h6'}>
-            CAESAR CIPHER KEY
-          </Typography>
-          <hr />
-        </Grid>
-        <Grid item xs={12}>
-          <label for="exampleInputEmail1">Enter a caesar cipher key</label>
-          <input
-            type="number"
-            className="form-control"
-            id="caesar_key"
-            name="caesar_key"
-            placeholder="Caesar Cipher Key"
-            style={{ margin: "0.75rem 0 0.75rem 0" }}
-          />
-        </Grid>
-        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-          <Button
-            type="submit"
-            variant="contained"
-          >
-            Register
-          </Button>
-        </div>
-      </Box>
+          <Paper sx={{ p: 2, mt: 2 }}>
+            <Grid item xs={12}>
+              <Grid item xs={12}>
+                <Typography textAlign={'center'} variant={'h6'} sx={{ backgroundColor: 'lightblue' }}>
+                  SECURITY QUESTIONS
+                </Typography>
+                <hr />
+              </Grid>
+              <label>What is the name of your favorite color?</label>
+              <input
+                className="form-control"
+                type="text"
+                id="answer1"
+                name="answer1"
+                required
+                placeholder="Favorite Color"
+                style={{ margin: "0.75rem 0 0.75rem 0" }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <label>Who is your role model?</label>
+              <input
+                className="form-control"
+                type="text"
+                id="answer2"
+                name="answer2"
+                required
+                placeholder="Role Model"
+                style={{ margin: "0.75rem 0 0.75rem 0" }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <label>Who is your favorite player?</label>
+              <input
+                className="form-control"
+                type="text"
+                id="answer3"
+                name="answer3"
+                required
+                placeholder="Favorite Player"
+                style={{ margin: "0.75rem 0 0.75rem 0" }}
+              />
+            </Grid>
+          </Paper>
+
+          <Paper sx={{ p: 2, mt: 2 }}>
+            <Grid item xs={12}>
+              <Typography textAlign={'center'} variant={'h6'} sx={{ backgroundColor: 'lightblue' }}>
+                CAESAR CIPHER KEY
+              </Typography>
+              <hr />
+            </Grid>
+            <Grid item xs={12}>
+              <label for="exampleInputEmail1">Enter a caesar cipher key</label>
+              <input
+                type="number"
+                className="form-control"
+                id="caesar_key"
+                name="caesar_key"
+                placeholder="Caesar Cipher Key"
+                style={{ margin: "0.75rem 0 0.75rem 0" }}
+              />
+            </Grid>
+          </Paper>
+          <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+            <Button
+              type="submit"
+              variant="contained"
+            >
+              Register
+            </Button>
+          </div>
+        </Box>
+      </Paper>
     </Container >
   );
 };
