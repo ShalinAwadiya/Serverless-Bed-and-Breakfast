79a592b6f276219df231b68b051513b09080a722
Dhruvkumar Patel <dh265788@dal.ca>
2022-07-18 15:06:32 -0300

    Revert "Merge branch 'chat-rel' into 'main'"
    This reverts merge request !14

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,8 +13,8 @@ import { Protected } from './views/authentication/Protected';
 import LoginStage3 from './views/authentication/Login-3';
 import Summary from './views/Summary/Summary';
 import Food from './views/Food/Food';
-import ChatSupport from './views/support/ChatSupport';
-import Chat from './views/chatbot/Chat';
+
+// import Chat from './views/chatbot/Chat';
 import Admin from './views/admin';
 import ChatSupport from './views/support/ChatSupport';
 import DisplayTour from './views/tour/DisplayTour';
