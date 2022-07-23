7ded32a52b65e56314b65fc9b04f15154a75e118
Minal Khona <mn977442@dal.ca>
2022-07-23 01:54:28 -0300

    Formatted Invoice

--- frontend/src/views/admin/Invoice.js
+++ frontend/src/views/admin/Invoice.js
@@ -1,58 +1,56 @@
-import React,{useEffect,useState} from "react";
+import { Container, Paper, Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 const Invoice = () => {
   const location = useLocation();
   const item = location.state.invoice;
   const index = location.state.index;
-  const [total,setTotal] = useState(0);
-  useEffect(()=>{
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
     console.log(location.state)
-    item.food.forEach(element => {
-        setTotal(old => old+(element.quantity * element.price))
-    });
-  },[])
+    setTotal(item.totalPrice)
+  }, [])
   return (
-    <div class="card">
-      <div class="card-body mx-4">
-        <div class="container">
-          <p class="my-5 mx-5" style={{ "font-size": "30px" }}>
-            Thank for your purchase
-          </p>
-          <div class="row">
-            <ul class="list-unstyled">
-              <li class="text-black">{item.email}</li>
-              <li class="text-muted mt-1">
-                <span class="text-black">Invoice</span> #{index+1}
-              </li>
-              <li class="text-black mt-1">{item.date}</li>
-            </ul>
-            <hr />
-            {item.food.map((element) => {
-              return (
-                <>
-                  <div class="col-xl-10">
+    <div class="container">
+      <p class="my-4 mx-5" style={{ "font-size": "30px" }}>
+        Thank for your purchase
+      </p>
+      <div class="row">
+        <Stack direction={'column'} spacing={2}>
+          <Stack>
+            <Typography>{item.email}</Typography>
+          </Stack>
+          <Stack>
+            <Typography>{item.date}</Typography>
+          </Stack>
+        </Stack>
+        <hr />
+        {item.food.map((element) => {
+          return (
+            <Container maxWidth="xs" sx={{ p: 4 }}>
+              <Paper sx={{ p: 2 }}>
+                <Stack direction={'column'} spacing={2}>
+                  <Stack direction={'row'} spacing={30}> <Stack>
                     <p>{element.name}</p>
-                  </div>
-                  <div class="col-xl-2">
-                    <p class="float-end">{element.price}</p>
-                  </div>
-                </>
-              );
-            })}
-
-            
-            <hr />
-          
-             <hr style={{ border: "2px solid black" }} />
-          </div>
-          <div class="row text-black">
-            <div class="col-xl-12">
-              <p class="float-end fw-bold">{total}</p>
-            </div>
-            <hr style={{ border: "2px solid black" }} />
-          </div>
-          
-        </div>
+                  </Stack>
+                    <Stack>
+                      <p>${element.price}</p>
+                    </Stack>
+                  </Stack>
+                </Stack>
+              </Paper>
+            </Container>
+          );
+        })}
+        <hr />
+        <Stack direction={'row'} spacing={10} sx={{ display: 'flex', justifyContent: 'right' }}>
+          <Stack>
+            <p>Total Cart Price</p>
+          </Stack>
+          <Stack>
+            ${total}
+          </Stack>
+        </Stack>
       </div>
     </div>
   );
